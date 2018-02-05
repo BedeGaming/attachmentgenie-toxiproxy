22d5f570cd1cfe2c936631c419e0997c2404a2f6
Bram Vogelaar <attachmentgenie@inuits.eu>
2018-02-05 01:24:04 +0100

    appease the lint gods

--- test/integration/default/toxiproxy_spec.rb
+++ test/integration/default/toxiproxy_spec.rb
@@ -3,9 +3,9 @@ control 'jobmanager 01' do
   title 'toxiproxy jobmanager service is running'
   desc 'Ensures that the toxiproxy jobmanager service is up and running'
   describe service('toxiproxy') do
-    it { should be_enabled }
-    it { should be_installed }
-    it { should be_running }
+    it { is_expected.to be_enabled }
+    it { is_expected.to be_installed }
+    it { is_expected.to be_running }
   end
 end
 
@@ -14,8 +14,8 @@ control 'jobmanager 02' do
   title 'toxiproxy jobmanager service is listening at port 8474'
   desc 'Ensures that the toxiproxy jobmanager service is listening at port 8474'
   describe port(8474) do
-    it { should be_listening }
-    its('processes') { should include 'toxiproxy'}
-    its('protocols') { should include 'tcp' }
+    it { is_expected.to be_listening }
+    its('processes') { is_expected.to include 'toxiproxy' }
+    its('protocols') { is_expected.to include 'tcp' }
   end
 end
